9fbac6c082a19d667a2aad632a867731affa9dbc
kenice washington <kenicek56@gmail.com>
2014-10-29 12:27:43 -0700

    i finished video 13.

--- controller/create-db.php
+++ controller/create-db.php
@@ -1,26 +1,29 @@
 <?php
-
+// access my model
 require_once(__DIR__ . "/../model/database.php");
 
 $connection = new mysqli($host, $username, $password);
 
 if($connection->connect_error ){
-
-die("Error: " . $connection->connect_error);	
+	die("Error: " . $connection->connect_error);	
 }
 $exists = $connection->select_db($database);
 
 if(!$exists){
 	// sends commands to the database
 	$query = $connection->query("CREATE DATABASE $database");
-	
-// checks if our query is succesful
-if($query){
 
-echo "Successfully created database " . $database;
+// checks if our query is successful
+	if($query){
 
+	echo "Successfully created database: " . $database;
+
+	}
 }
 
+//this else echos that my database is created
+else {
+	echo "Database already exists";
 }
 
 $connection->close();
