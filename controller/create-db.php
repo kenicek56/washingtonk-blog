e27cbb3edad3a11c0a633a964f76ac90025dccd2
kenice washington <kenicek56@gmail.com>
2014-10-30 12:15:40 -0700

    finished video 14

--- controller/create-db.php
+++ controller/create-db.php
@@ -31,6 +31,14 @@ if(!$exists){
 else {
 	echo "Database already exists";
 }
+// creates a blog post
+$query = $connection->query("CREATE TABLE posts (" 
+	// null indicates that when ever we create a new blog post the id cant be known and id cant be empty.
+	. "id int (11) NOT NULL AUTO_INCREMENT,"
+	. "title varchar(255) NOT NULL,"
+	. "post text NOT NULL,"
+	. "PRIMARY KEY (id))");
+// 1 means there are 11 values.
 
 $connection->close();
 
