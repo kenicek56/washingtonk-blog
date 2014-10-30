a96869d34910f4bdd65a9de85d5ccad1966b656d
kenice washington <kenicek56@gmail.com>
2014-10-30 11:43:47 -0700

    I made a crap load of comments.

--- controller/create-db.php
+++ controller/create-db.php
@@ -2,10 +2,14 @@
 // access my model
 require_once(__DIR__ . "/../model/database.php");
 
-// gives us the nessesary function so we can use our mysqli
+// gives us the nessesary function so we can use our mysqli.
 // new is a keyword that allows us to build objects within php.
+// mysqli is used to connect our objects to the database.
 $connection = new mysqli($host, $username, $password);
 
+// to see if the connection was succesful
+// to check if its true or nah
+// connection->connect_error checks to see if there is a connection error.
 if($connection->connect_error ){
 	die("Error: " . $connection->connect_error);	
 }
