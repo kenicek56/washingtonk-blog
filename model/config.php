69d9adb309ae179b5f6502a3bc3f0cc12453baf7
kenice washington <kenicek56@gmail.com>
2014-11-04 12:22:44 -0800

    i connected the post.php to my index in from video 18

---
+++ model/config.php
@@ -0,0 +1,3 @@
+<?php
+
+$path = "/washingtonk-blog/";
--- view/navigation.php
+++ view/navigation.php
@@ -1,6 +1,13 @@
+<?php
+//DIR to get out of the folder and to get into another folder and get into the file i want
+ require_once(__DIR__ . "/../model/config.php");
+?>
+
 <nav>
 	
 	<ul>
-		<li> <a href="../post.php">Blog post form</a></li>
+	 <!--echoing out the right path to the post.php -->
+	 <!--were echoing out a string -->
+		<li> <a href="<?php echo $path . "post.php"?>">Blog Post Form</a></li>
 	</ul>
 </nav>
