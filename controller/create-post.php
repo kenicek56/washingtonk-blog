2c5e309335dd66617e2febd2ba26371999aabe6b
kenice washington <kenicek56@gmail.com>
2014-11-12 12:26:34 -0800

    i finished video 22 and 23 and i connected my queries and and made a form to send information and to gain info.

--- controller/create-post.php
+++ controller/create-post.php
@@ -1,7 +1,14 @@
 <?php
+require_once(__DIR__ . "/../model/database.php");
+// runs our query
+$connection = new mysql($host, $username, $password, $database);
+
 // validates the input.
 // tells the input that we want to see the input.
-$title =filer_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
+$title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
+$post = filter_input(INPUT_POST, "post",FILTER_SANITIZE_STRING);
 
 echo "<p>Title: $title</p>";
 echo "<p>Post: $post</p>"
+// closes the connection to the database
+$connection->close();
--- view/form.php
+++ view/form.php
@@ -1,6 +1,10 @@
-<h1>Create Blog Post</h1>
+<?php
+require_once (__DIR__ . "/../model/config.php");
+?>
 
-<form>
+<h1>Create Blog Post</h1>
+<!-- to send information or to gin imformation-->
+<form method="post" action="<?php echo $path . "controller/create-post.php"; ?>">
 	<div>
 		
 			<label for="title">Title: </label>
