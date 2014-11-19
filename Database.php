291f59417df453d48abac3f1fdb4f769c4a1d956
kenice washington <kenicek56@gmail.com>
2014-11-18 20:33:25 -0800

    finished video 28 and i added functions to my database.php.

---
+++ Database.php
@@ -0,0 +1,41 @@
+<?php
+class Database {
+	private $connection;
+	private $host;
+	private $username;
+	private $password;
+	private $database;
+
+// local variables that will disappear once the function is finished running
+// a constructor can accept parameters in the normal manner, which are passed when the object is created
+//defines the constructors.
+//constructor is called on an object after it has been created	
+	public function __construct($host, $username, $password, $database) {
+		//accesses to our golbal variables
+		$this->host =$host;
+		$this->username = $username;
+		$this->password = $password;
+		$this->database = $database;
+
+
+	}
+	public function openConnection() {
+ 
+ 
+
+	}
+	public function closeConnection() {
+
+	}
+	// everytime we call on our query function we have to passed in a string and the string can be located in the variable
+	public function query($string) {
+
+	}
+}
+//It's a way to view your code in a more intuitive, real-world way
+//its so that you can customize the behavior of that set of functions
+//The point of OOP is not to 'group similar functions in a class
+//in a class context, it is possible to create a new object by new self and new parent
+//we make a class to call it later
+//when we create an instance of a class we create an object
+// when you make functions you have to call every function over and over.
