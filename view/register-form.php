348e17033a1af3c04fd575ca5662e46f11d4b4a1
kenice washington <kenicek56@gmail.com>
2014-12-04 21:20:11 -0800

    added a controller to my register form.

--- view/register-form.php
+++ view/register-form.php
@@ -1,4 +1,10 @@
-<form>
+<?php
+require_once(__DIR__ . "/../model/config.php");
+?>
+
+<h1>Register</h1>
+
+<form method="post" action="<?php echo $path . "controller/create-user.php"; ?>">
 	
 <div>
 	<!--creates a slot for the users email-->
