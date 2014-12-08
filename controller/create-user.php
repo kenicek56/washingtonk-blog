05d7a30cd826edb38ed15ee17477d79eb394362b
kenice washington <kenicek56@gmail.com>
2014-12-07 20:59:53 -0800

    created a salt for my password

--- controller/create-user.php
+++ controller/create-user.php
@@ -7,3 +7,10 @@ $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
 $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING);
 
 echo $email . " - " . $username . " - " . $password;
+
+//
+// tells php that we wanna use salt 56 5,000 times
+//creates unqiue id for us
+$salt = "$5$" . "round=5000$" . uniqid(mt_rand(), true) . "$";
+
+echo $salt;
