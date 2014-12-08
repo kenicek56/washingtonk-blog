7e936fd2cb2a5c33512ccf21e2d8368f9dbdc37d
kenice washington <kenicek56@gmail.com>
2014-12-07 22:05:28 -0800

    finished video 45 and made my post visible on my index.

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
       require_once(__DIR__ . "/controller/create-db.php");
        require_once (__DIR__ . "/view/footer.php");
        require_once(__DIR__ . "/model/date.php");
+       require_once(__DIR__ . "/controller/read-posts.php");
 ?>
 
 <!DOCTYPE html>
