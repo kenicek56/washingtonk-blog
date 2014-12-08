fe357f7ec25cea0ee4f6910fad61093230eab221
kenice washington <kenicek56@gmail.com>
2014-12-07 20:52:11 -0800

    finished video 42 and stored my register form.

--- controller/create-user.php
+++ controller/create-user.php
@@ -1,2 +1,9 @@
 <?php
-echo "successfully created user";
+require_once(__DIR__ . "/../model/config.php");
+// deletes any characters it doesnt recoginze as part of the string 
+//deletes any characters that dont exsist in email
+$email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
+$username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
+$password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING);
+
+echo $email . " - " . $username . " - " . $password;
