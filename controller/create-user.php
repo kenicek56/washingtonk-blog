2742b8e9ce28a0fe8ffaa46ad0f7e3614d1a7f67
kenice washington <kenicek56@gmail.com>
2014-12-07 21:23:08 -0800

    finished video 44 and i encrypted my password and stored it in the Database.

--- controller/create-user.php
+++ controller/create-user.php
@@ -6,11 +6,25 @@ $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
 $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
 $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING);
 
-echo $email . " - " . $username . " - " . $password;
+//echo $email . " - " . $username . " - " . $password;
 
 //
 // tells php that we wanna use salt 56 5,000 times
 //creates unqiue id for us
-$salt = "$5$" . "round=5000$" . uniqid(mt_rand(), true) . "$";
+$salt = "$5$" . "rounds=5000$" . uniqid(mt_rand(), true) . "$";
 
-echo $salt;
+// tells it to use this password and this salt to come together to create a incrypted password.
+$hashedPassword = crypt($password, $salt);
+
+$query = $_SESSION["connection"]->query("INSERT INTO users Set "
+	. "email = '$email',"
+	. "username = '$username',"
+	. "password = 'hashedPassword',"
+	. "salt = '$salt'");
+
+if($query) {
+	echo "Successfully created user: $username";
+}
+else {
+	echo "<p>" . $_SESSION["connection"]->error . "</p>";
+}
