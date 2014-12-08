d72ff4f9d34287e24b7fe3e654af662914969796
kenice washington <kenicek56@gmail.com>
2014-12-07 22:17:27 -0800

    finished video 47 and selected user info from my database!

--- controller/login-user.php
+++ controller/login-user.php
@@ -1,2 +1,7 @@
 <?php
-echo "Successfully logged in user";
+require_once(__DIR__ . "/../model/config.php");
+
+$username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
+$password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING);
+// selects our salt and password from our users table where our usersname is = to the username tht the user input
+$query = $_SESSION["connection"]->query("SELECT  salt, password FROM users WHERE username = '$username'");
