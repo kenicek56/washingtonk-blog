a97e4ded44943ac686d4c38e0cce9112ea1c26fe
kenice washington <kenicek56@gmail.com>
2014-12-09 19:24:41 -0800

    finished video 48 and checked the passwords for login.

--- controller/login-user.php
+++ controller/login-user.php
@@ -5,3 +5,25 @@ $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
 $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING);
 // selects our salt and password from our users table where our usersname is = to the username tht the user input
 $query = $_SESSION["connection"]->query("SELECT  salt, password FROM users WHERE username = '$username'");
+
+if($query->num rows == 1) {
+
+	$row = $query->fetch_array();
+
+	if($row["password"] === crypt($password, $row["salt"])) {
+
+		echo "<p>Login Successful!</p>";
+    }
+
+    else {
+
+         echo "<p>Invalid username and password</p>";
+
+    }
+}
+
+else {
+
+echo "<p> Invalid username and password</p>";
+
+}
--- index.php
+++ index.php
@@ -2,12 +2,12 @@
       // connects to the header
       require_once (__DIR__ . "/view/header.php");
       // connects
-       require_once (__DIR__ . "/view/navigation.php");
+      require_once (__DIR__ . "/view/navigation.php");
       // connects to create-db.php
       require_once(__DIR__ . "/controller/create-db.php");
-       require_once (__DIR__ . "/view/footer.php");
-       require_once(__DIR__ . "/model/date.php");
-       require_once(__DIR__ . "/controller/read-posts.php");
+      require_once (__DIR__ . "/view/footer.php");
+      require_once(__DIR__ . "/model/date.php");
+      require_once(__DIR__ . "/controller/read-posts.php");
 ?>
 
 <!DOCTYPE html>
