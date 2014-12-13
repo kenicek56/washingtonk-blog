0e84854557c8ea8d68addd37495c3f910f91b122
kenice washington <kenicek56@gmail.com>
2014-12-13 00:39:51 -0800

    im trying to style my blog

--- controller/read-posts.php
+++ controller/read-posts.php
@@ -1,3 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+      <title></title>
+      <!--connects to bootstrap and the main.css-->
+     
+    
+</head>
+<header>
+      
+</header>
+<body>
+</body>
+</html>
 <?php
 
 require_once(__DIR__ . "/../model/config.php");
@@ -8,13 +22,29 @@ $result = $_SESSION["connection"]->query($query);
 
 if ($result) {
 	while ($row = mysqli_fetch_array($result)) {
-		echo "<div class='post'>";
+		echo "<div class='container'>";
+		echo "<div class='row'>";
+		echo "<div id='title' class='Blog-Project col-xs-12'>";
+		echo "<h1>Blog Project</h1>";
+		echo "</div>";
+        echo "</div>";
+		echo "<div class='row'>";
+		echo "<div class = 'post col-xs-7'>";
 		echo "<h2>" . $row['title'] . "</h2>";
 		echo "<p>" . $row['DateTime'] . "</p>";
 		echo "<br />";
 		echo "<p>" . $row['post'] . "</h1>";
 		echo "<br/>";
 		echo "</div>";
+		echo "<div id='title' class='About-Me col-xs-4'>";
+		echo "<h1>About-Me</h1>";
+        echo "</div>";
+		echo "</div>";
+		echo "</div>";
+		echo "</div>";
+		
+
+
 
 	}
 
